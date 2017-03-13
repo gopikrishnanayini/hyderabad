040109815637d641ae5fe4dcb18f8c9ebb8af219
gopikrishna <nayinigopi93@gmail.com>
2017-03-13 23:27:56 +0530

    one to one association

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,34 +1,58 @@
 class LocationsController < ApplicationController
   def index
-  	@locations = Location.all
+    @event = Event.find(params[:event_id])
+    @locations = @event.location
   end
   def new
   	@event = Event.find(params[:event_id])
-  	#binding.pry
-  	@location = @event.build_location
+    @location = @event.build_location
   end
-
+  
   def create
-  	#binding.pry
   	@event = Event.find(params[:event_id])
-  	#inding.pry
   	@location = @event.build_location(location_params)
-  	respond_to do |format|
+      respond_to do |format|
       if @location.save
-        format.html { redirect_to event_locations_path(@event), notice: 'event was successfully created.' }
-        #format.json { render json: @location, status: :created, location: @location }
+        format.html { redirect_to ([@event, @location]), notice: 'event was successfully created.' }
+        format.json { render json: @location, status: :created, location: @location }
       else
         format.html { render action: "new" }
-        #format.json { render json: @location.errors, status: :unprocessable_entity }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
       end
     end
   end
   def show
-    @location = Location .find(params[:id])
+    @event = Event.find(params[:event_id])
+    @location = @event.location
   end
 
+  def edit
+    @event = Event.find(params[:event_id])
+    @location = @event.location
+  end
+
+  def update
+    #binding.pry
+    @event = Event.find(params[:event_id])
+    #inding.pry
+    @location = @event.location
+    respond_to do |format|
+      if @location.update(location_params)
+        format.html { redirect_to ([@event, @location]), notice: 'event was successfully created.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "update" }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+     redirect_to locations_path
+  end
   private
   def location_params
-  	params.require(:location).permit(:address, :event_id)
+    params.require(:location).permit(:address, :event_id)
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
 	belongs_to :event
+	validates :address, presence: true
 end
